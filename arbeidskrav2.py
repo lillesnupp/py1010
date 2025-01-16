b3eb77ee6c9fd8235d2fd70065501ac7b602fd58
Anne-Marie <myhrvolda@gmail.com>
2025-01-17 00:09:00 +0100

    Update arbeidskrav2.py

--- arbeidskrav2.py
+++ arbeidskrav2.py
@@ -6,9 +6,7 @@ Arbeidskrav 2 - PY1010-1 24H Grunnleggende programmering med Python
 @author: Anne-Marie Myhrvold
 """
 
-# Oppg 1) Du skal her lage et program som skal starter med 
-# alder = int(input('Hvilket år er du født? ') )
-# Programmet skal så regne ut hvor gammel personen blir nå i løpet av år 2024 og skrive svaret til skjerm med passende tekst
+# Oppg 1) Program som regner ut hvor gammel en person er i 2024 og skriver ut resultatet
 
 # Be om fødselsår:
 alder = int(input('Hvilket år er du født? ') )
